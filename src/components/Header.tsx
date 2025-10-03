515e163097fa00fe1da83fecdafb2b5b7585d37e
chapetta <cha.petta@hotmail.com>
2025-10-03 19:03:26 -0300

    criando o header e os links e implementando no componente layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <header className="bg-violet-500 px-50 h-20 flex justify-end items-center">
+          <nav>
+            <ul className="flex gap-10 text-2xl justify-end mx-5">
+              <li>Página Inicial</li>
+              <li>Sobre</li>
+              <li>Portfólio</li>
+              <li>Contato</li>
+            </ul>
+          </nav>
+        </header>
+    );
+};
