919e409aa294409fa58748a3ea62195ca8f3bd10
chapetta <cha.petta@hotmail.com>
2025-10-03 18:54:36 -0300

    começando a implementar a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,14 @@
+import Lottie from 'lottie-react'
+import codingAnimation from '../../assets/coding.json'
+
 export const Home = () => {
-    return (
-        <div>Home</div>
-    );
+  return (
+    <main className="flex text-6xl justify-around items-center">
+      <div>
+        <h1>Desenvolvedor Web</h1>
+        <h1>FullStack</h1>
+      </div>
+      <Lottie animationData={codingAnimation} loop={true} />
+    </main>
+  );
 };
